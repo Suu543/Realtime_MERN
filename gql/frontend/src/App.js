9a6f43aacbab0c33dd7f55c9e006449e45e2015c
YONGSU JEONG <jos50275266@gmail.com>
2021-01-06 17:09:31 +0900

    Part-2 GraphQL - React useQuery hook from @apollo/client

--- gql/frontend/src/App.js
+++ gql/frontend/src/App.js
@@ -1,46 +1,18 @@
 import React, { useState } from 'react';
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client"
+import { ApolloClient, InMemoryCache, gql, useQuery, ApolloProvider } from "@apollo/client"
+import Home from "./pages/Home"
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
   cache: new InMemoryCache()
-})
+});
 
 const App = () => {
 
-  const [posts, setPosts] = useState([]);
-
-  client.query({
-    query: gql`
-      {
-        allPosts {
-          id
-          title
-          description
-        }
-      }
-    `
-  }).then(result => setPosts(result.data.allPosts));
-
   return (
-    <div className="container p-5">
-      <div className="row p-5">
-        {posts.map((p) => (
-          <div className="col-md-4" key={p.id}>
-            <div className="card">
-                <div className="card-body">
-                  <div className="card-title">
-                    <h4>{p.title}</h4>
-                  </div>
-                  <p className="card-text">
-                    {p.description}
-                  </p>
-                </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <ApolloProvider client={client}>
+      <Home />
+    </ApolloProvider>
   );
 }
 

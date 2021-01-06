670af231aa9b2186441ba8a79aa3a96fd4633206
YONGSU JEONG <jos50275266@gmail.com>
2021-01-06 11:46:14 +0900

    GraphQL Express(REST) Server for testing purpose

--- gql/backend/server.js
+++ gql/backend/server.js
@@ -1,8 +1,36 @@
+const { ApolloServer } = require("apollo-server-express");
+const http = require("http");
 const express = require('express');
 require('dotenv').config();
 
 // Invoke Express
 const app = express();
+
+const typeDefs = `
+    type Query {
+        totalPosts: Int!
+    }
+`;
+
+// resolvers
+const resolvers = {
+    Query: {
+        totalPosts: () => 42
+    }
+};
+
+// graphql server
+const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+// Apply Middleware method to connects ApolloServer to a specific HTTP Framework ie: express
+// this express server itself applies for graphql server
+apolloServer.applyMiddleware({ app });
+
+// server - Pass this express server to this http server
+// const httpServer = http.createServer(app);
 
 // Rest Endpoint
 app.get('/rest', (req, res) => {
@@ -13,5 +41,6 @@ app.get('/rest', (req, res) => {
 
 // PORT
 app.listen(process.env.PORT, () => {
-    console.log(`Listening on PORT http://localhost:${process.env.PORT}`);
+    console.log(`server is listening on PORT http://localhost:${process.env.PORT}`);
+    console.log(`graphql server is listening on PORT http://localhost:${process.env.PORT}${apolloServer.graphqlPath}`);
 });

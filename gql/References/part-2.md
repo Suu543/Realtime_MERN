e735550d1ca24db746f91724f458db390c950491
YONGSU JEONG <jos50275266@gmail.com>
2021-01-12 16:34:55 +0900

    Part-3 Firebase Setup

---
+++ gql/References/part-2.md
@@ -0,0 +1,8 @@
+Firebase and React
+<img src="https://cdn-images-1.medium.com/max/800/1*vdLSfGC-Qn8umJ0kxtmkSg.png" />
+- https://console.firebase.google.com/u/0/?hl=ko
+
+```javascript
+frontend - npm i firebase
+```
+<img src="https://cdn-images-1.medium.com/max/800/1*rQkVHO9HFH7GcWQHYcY32Q.png" />
---
+++ gql/frontend/src/firebase.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyAGAK8VnJt6-w-Uc5VcpaA3peNqDtFpG90",
+    authDomain: "gqlreactnode-812bb.firebaseapp.com",
+    projectId: "gqlreactnode-812bb",
+    storageBucket: "gqlreactnode-812bb.appspot.com",
+    // messagingSenderId: "94643704174",
+    appId: "1:94643704174:web:5e09a1351dbbf857ceb17f",
+    measurementId: "G-V02QSD26ME"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+export const auth = firebase.auth()
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider()

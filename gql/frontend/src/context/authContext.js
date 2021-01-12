d253d99ab8b9931430480af56c947ed3ef101e07
YONGSU JEONG <jos50275266@gmail.com>
2021-01-12 16:46:02 +0900

    Part-3 State Management with context and reducer hooks

---
+++ gql/frontend/src/context/authContext.js
@@ -0,0 +1,30 @@
+import React, { useReducer, createContext } from 'react'
+
+// Reducer - Responsible for updating context
+const firebaseReducer = (state, action) => {
+    switch(action.type) {
+        case "LOGGED_IN_USER":
+            return { ...state, user: action.payload }
+        default:
+            return state;
+    }
+}
+
+// state
+const initialState = {
+    user: null
+}
+
+// Create Context
+const AuthContext = createContext();
+
+// Context Provider
+const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(firebaseReducer, initialState);
+    
+    const value = {state, dispatch}
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
+
+// Export
+export { AuthContext, AuthProvider }

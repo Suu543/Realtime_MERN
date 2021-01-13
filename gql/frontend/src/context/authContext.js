4ce25baae6b29873e2ab42a364e550ea603149f4
YONGSU JEONG <jos50275266@gmail.com>
2021-01-13 23:44:50 +0900

    Part-3 Logout from firebase

--- gql/frontend/src/context/authContext.js
+++ gql/frontend/src/context/authContext.js
@@ -1,4 +1,5 @@
-import React, { useReducer, createContext } from 'react'
+import React, { useEffect, useReducer, createContext } from 'react'
+import { auth } from "../firebase";
 
 // Reducer - Responsible for updating context
 const firebaseReducer = (state, action) => {
@@ -21,6 +22,31 @@ const AuthContext = createContext();
 // Context Provider
 const AuthProvider = ({ children }) => {
     const [state, dispatch] = useReducer(firebaseReducer, initialState);
+
+    // User onAuthStateChanged
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(async user => {
+            if (user) {
+                const idTokenResult = await user.getIdTokenResult();
+
+                dispatch({
+                    type: "LOGGED_IN_USER",
+                    payload: { email: user.email, token: idTokenResult.token }
+                });
+            } else {
+                dispatch({
+                    type: 'LOGGED_IN_USER',
+                    payload: null
+                })
+            }
+        });
+
+        // Cleanup - the purpose of return in useEffect
+        // AuthState가 변한 것을 반영하기 전
+        return () => unsubscribe();
+
+    }, [])
+
     
     const value = { state, dispatch }
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;

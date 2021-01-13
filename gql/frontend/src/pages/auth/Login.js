d41da270838cbf17c83a033e4df45d57b6c15ce4
YONGSU JEONG <jos50275266@gmail.com>
2021-01-13 15:57:58 +0900

    Part-3 User Register Form

--- gql/frontend/src/pages/auth/Login.js
+++ gql/frontend/src/pages/auth/Login.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleSubmit = () => {
+    }
+
     return (
-        <div className="container">
-            <div className="row p-5">
+        <div className="container p-5">
                 <h4>Login</h4>
-            </div>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label>Email Address</label>
+                        <input 
+                            className="form-control" 
+                            type="email" 
+                            value={email} 
+                            placeholder="Enter Email"
+                            onChange={(e) => setEmail(e.target.value)} 
+                            disabled={loading}
+                        />
+                    </div>
+                </form>
         </div>
     )
 }
